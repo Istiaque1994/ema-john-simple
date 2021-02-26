a794a26085b9f9623b4ddf3f15b77c3e352a5cf5
Istiaque1994 <istiqueahmed80@gmail.com>
2021-02-26 12:40:27 +0600

    Some Bootstrap Add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import './Header.css';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
     return (
         <div className="header">
-            <img src={logo} alt=""/>
-            <nav>
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/anage">Manage Inventory here</a>
-            </nav>
+            <div className="logo">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <img src={logo} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="menu">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <nav>
+                                <a href="/shop">Shop</a>
+                                <a href="/review">Order Review</a>
+                                <a href="/anage">Manage Inventory here</a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
